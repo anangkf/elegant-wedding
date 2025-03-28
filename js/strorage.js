9ede6165f8e6052efe0ac858d39ba7f1c8b253d0
anangkf <anangkhoirulfadli@gmail.com>
2025-03-28 08:34:03 +0700

    moved storage from local storage to json bin

--- js/strorage.js
+++ js/strorage.js
@@ -1,3 +1,32 @@
+// const binId = "67e5f6488a456b79667e11b5"; //dev 
+const binId = "67e5fc2c8960c979a579dcf6"; //prod
+const apiKey = "$2a$10$Ffq5aoPmc7xeKTN2B9Y6RuL.fpVuxP9m/KLrStY05A9k25VKCvogG"; 
+const url = `https://api.jsonbin.io/v3/b/${binId}`;
+
+// GET Data
+async function getMessages() {
+  let response = await fetch(url, {
+    headers: { "X-Master-Key": apiKey }
+  });
+  let data = await response.json();
+  return data.record.messages || [];
+}
+
+// POST Data
+async function saveMessage({messages, newMessage}) {
+  // let messages = await getMessages();
+  messages.push(newMessage);
+  await fetch(url, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key": apiKey
+    },
+    body: JSON.stringify({ messages })
+  });
+}
+
+
 // method
 function insert() {
   const form = $("#formMessage").serializeArray();
@@ -9,7 +38,7 @@ function insert() {
     let value = name === "id" || name === "" ? Number(item["value"]) : item["value"];
     newData[name] = value;
   });
-  // console.log(newData)
+
   localStorage.setItem("dataMessage", JSON.stringify([...dataMessage, newData]));
   return newData;
 }
@@ -30,15 +59,25 @@ function showData(dataMessage) {
 }
 
 let dataMessage;
-$(function () {
+$(async function () {
   // initialize
-  dataMessage = JSON.parse(localStorage.getItem("dataMessage")) || [];
+  dataMessage = await getMessages() || [];
 
   $(".card-message").html(showData(dataMessage));
   // events
-  $("#formMessage").on("submit", function (e) {
+  $("#formMessage").on("submit", async function (e) {
     e.preventDefault();
-    dataMessage.push(insert());
+
+    const formData = new FormData(e.target)
+    const body = {
+      nama: formData.get('nama'),
+      hubungan: formData.get('hubungan'),
+      pesan: formData.get('pesan')
+    }
+
+    await saveMessage({messages: dataMessage, newMessage: body})
+    e.target.reset();
+    
     Swal.fire({
       position: "center",
       icon: "success",
